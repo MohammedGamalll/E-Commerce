48cc53fffc4f92bf89456b43430b6a9ae4d69ef3
MohammedGamalll <11410120220283@stud.cu.edu.eg>
2025-03-01 02:07:34 +0200

    refactor token interceptor logic; update not found page SVG dimensions for better responsiveness

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -3,6 +3,13 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject, PLATFORM_ID } from '@angular/core';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  if (
+    !req.url.includes('order') &&
+    !req.url.includes('cart') &&
+    !req.url.includes('wishlist')
+  ) {
+    return next(req);
+  }
   const _PLATFORM_ID = inject(PLATFORM_ID);
   if (isPlatformBrowser(_PLATFORM_ID)) {
     const token = localStorage.getItem('token');
